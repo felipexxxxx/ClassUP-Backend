7e8a6118179e533a0e5cebf5011c647276405ba3
Felipe Lins <felipeplinsesilva@gmail.com>
2025-03-21 14:05:08 -0300

    first commit

---
+++ src/main/java/com/agendaedu/educional/EducionalApplication.java
@@ -0,0 +1,13 @@
+package com.agendaedu.educional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agendaedu/educional/EducionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agendaedu.educional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
