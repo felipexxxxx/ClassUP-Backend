9a83b46cada65280e5928f60b1e077d8813f63d7
Felipe Lins <felipeplinsesilva@gmail.com>
2025-03-22 15:15:21 -0300

    emailsender funcionou (dotenv tava quebrando o envio do email por algum motivo)

--- src/main/resources/db/migration/V5__create_notifications.sql
+++ src/main/resources/db/migration/V5__create_notifications.sql
@@ -2,7 +2,8 @@ CREATE TABLE notifications (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     usuario_id BIGINT NOT NULL,
     atividade_id BIGINT NOT NULL,
-    enviada BOOLEAN DEFAULT FALSE,
+    mensagem VARCHAR(255) NOT NULL,
+    data_envio DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (usuario_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (atividade_id) REFERENCES activities(id) ON DELETE CASCADE
 );
